2cb9c4bf4481c582c1dd20410121f3cc4211448d
utakatanoyume <igneeet@gmail.com>
2025-05-19 23:26:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function(){
+    $(window).scroll(function(){
+        // フェードイン要素の処理
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+
+        // スライドイン要素の処理
+        $('.slidein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
